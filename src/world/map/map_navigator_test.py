13199b1b7d3d1f45f6a1042fc14e60bb231f163e
JohnTrane <johntranee@gmail.com>
2021-09-09 00:53:58 -0700

    ud

--- src/world/map/map_navigator_test.py
+++ src/world/map/map_navigator_test.py
@@ -11,12 +11,25 @@ import time
 
 class MapNavigatorTest(unittest.TestCase):
 
-  def testDemoMinimap(self):
+  def testGoToBankeer(self):
     time.sleep(2)
     screen = take_screenshot()
     bank_label = IMAGE_MAP[ScreenPart.BANK_MINIMAP_LABEL]
-    center = area_of_picture(screen, bank_label).middle_x+5, area_of_picture(screen, bank_label).middle_y+5
-    pyautogui.click(center)
+    teleport_icon = IMAGE_MAP[ScreenPart.TELEPORT_MINIMAP_ICON]
+    first_town_icon = IMAGE_MAP[ScreenPart.FIRST_TOWN_TELEPORT_ICON]
+
+    teleport_icon_location = area_of_picture(screen, teleport_icon).middle_x, area_of_picture(screen, teleport_icon).middle_y
+    pyautogui.click(teleport_icon_location)
+    time.sleep(1)
+    screen = take_screenshot()
+
+    first_town_icon_location = area_of_picture(screen, first_town_icon).middle_x, area_of_picture(screen, first_town_icon).middle_y
+    pyautogui.click(first_town_icon_location)
+    time.sleep(25)
+    screen = take_screenshot()
+
+    bankeer_location_first_town = area_of_picture(screen, bank_label).middle_x+5, area_of_picture(screen, bank_label).middle_y+5
+    pyautogui.click(bankeer_location_first_town)
 
     # show_image_with_rectangle(screen, area_of_picture(screen, bank_label))
 
