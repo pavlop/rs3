0e13101b9f7a85c6a8c50f8d8933240fdc313b17
JohnTrane <johntranee@gmail.com>
2021-09-09 01:58:39 -0700

    ud

--- src/world/map/map_navigator_test.py
+++ src/world/map/map_navigator_test.py
@@ -23,19 +23,19 @@ class MapNavigatorTest(unittest.TestCase):
     return area_of_picture(take_screenshot(), icon) is not None
 
   def teleportTo(self, destination):
-    teleport_icon_location = area_of_picture(take_screenshot(), self.teleport_icon).middle_x, area_of_picture(
-      take_screenshot(), self.teleport_icon).middle_y
+    screen = take_screenshot()
+    teleport_icon_location = area_of_picture(screen, self.teleport_icon).middle_x, area_of_picture(screen, self.teleport_icon).middle_y
     pyautogui.click(teleport_icon_location)
     wait_until(self.isIconVisible(destination), 1, 0.1)
-    first_town_icon_location = area_of_picture(take_screenshot(), destination).middle_x, area_of_picture(
-      take_screenshot(), destination).middle_y
+    screen = take_screenshot()
+    first_town_icon_location = area_of_picture(screen, destination).middle_x, area_of_picture(screen, destination).middle_y
     pyautogui.click(first_town_icon_location)
     time.sleep(25)
 
   def navigateToBankeer(self):
     self.teleportTo(self.first_town_icon)
-    bankeer_location_first_town = area_of_picture(take_screenshot(), self.bank_label).middle_x + 5, area_of_picture(
-      take_screenshot(), self.bank_label).middle_y + 5
+    screen = take_screenshot()
+    bankeer_location_first_town = area_of_picture(screen, self.bank_label).middle_x+5, area_of_picture(screen, self.bank_label).middle_y+5
     pyautogui.click(bankeer_location_first_town)
 
 
